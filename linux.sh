5d7d49413ba50d2756bd2332e3d07a9957918c49
vot1291 <81410519+vot1291@users.noreply.github.com>
2021-03-26 15:33:30 +0700

    Add files via upload

---
+++ linux.sh
@@ -0,0 +1,59 @@
+#kode warna
+MERAH="\033[31;1m"
+HIJAU="\033[32;1m"
+
+
+echo -e $MERAH
+
+figlet -tc "HACK ACCOUNT ME"
+echo -e $HIJAU
+echo "JANGAN SALAH GUNAKAN TOOL INI"
+echo -e $MERAH
+echo "OKEH SELAMAT MENIKMATI TOOL INI"
+echo -e $HIJAU
+echo "MENU VIP HACK ACCOUNT AUTO PASSWORD REAL LINUX"
+echo -e $MERAH
+echo "VIP AUTO PASSWORD"
+sleep 0
+echo "VIP AUTO LOGIN"
+sleep 0
+echo "VIP AUTO MAIL PAS"
+echo "Just WhatsApp, my number is 085861120363"
+echo "==============================================="
+echo -e $HIJAU
+echo "MENU GRATIS HACK ACCOUNT "
+echo -e $MERAH
+echo "HACK FACEBOOK"
+echo "HACK INSTAGRAM"
+echo "HACK GOOGLE"
+echo "==============================================="
+read -p "PILIH MENU:"
+read -p "MASUKAN NAMA FACEBOK TARGET:"
+sleep 2
+echo "Memeriksa Nama Target"
+sleep 2
+echo "Nama target di temukan"
+sleep 2
+echo "Linux For andoird(2222)"
+sleep 2
+echo "Linux Program(123455)/"
+sleep 0
+echo "Linux TT7)/forget"
+sleep 2
+echo "tpe for facebbok..."
+sleep 0
+echo "ip Target 212.222.999.246."
+sleep 0
+echo "Login..."
+sleep 0
+echo "verifkasi "
+sleep 2
+echo "https://www.surveymonkey.com/welcome/sem/?program=7013A000000mweBQAQ&utm_bu=CR&utm_campaign=71                 >
+sleep 5
+echo -e $HIJAU
+
+figlet -tc "LINUX THE BEST"
+sleep 5
+
+figlet -tc "RIZAL NGENTOT"
+figlet -tc "PROGRAM NGEWE"
